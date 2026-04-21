d2aeadc9f763141295094a48ac45c40cabb873a7
Documenter.jl <documenter@juliadocs.github.io>
2026-04-21 06:11:07 +0000

    build based on 355c2e4

--- dev/search_index.js
+++
@@ -1,3 +0,0 @@
-var documenterSearchIndex = {"docs":
-[{"location":"#RCCL","page":"Home","title":"RCCL","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for RCCL.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
-}
